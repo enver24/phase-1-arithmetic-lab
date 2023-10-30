c9ceb88c8731e0f02e4e9e01b2d99e45c61fd392
Enver <mr_epsty@hotmail.com>
2023-10-30 16:45:40 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 19;
+const num4 = 5;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 10);
+const mod = num3 % num4;
+const max = Math.max(16, 17, 18 ,19, 20);
